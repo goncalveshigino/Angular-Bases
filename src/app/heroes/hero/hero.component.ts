b324f0a8c54c7fce095c17ed51e601cc91103bc1
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-09 10:58:26 +0100

    interpocao

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'Ironman';
+  public age: number = 45;
+
+}
